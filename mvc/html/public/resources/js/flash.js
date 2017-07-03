6dca43a92c7f5728b463137052dd17091fd35b9b
Sam Escolas <sescolas@student.42.us.org>
2017-07-03 02:38:34 -0400

    Added fadeout to flash messages

--- mvc/html/public/resources/js/flash.js
+++ mvc/html/public/resources/js/flash.js
@@ -1,8 +1,9 @@
 
 (function() {
 
+	var errors = document.getElementsByClassName("error");
+
 	function addListeners() {
-		var errors = document.getElementsByClassName("error");
 		for (var i=0; i<errors.length; i++) {
 			console.log("Adding listener to " + errors[i]);
 			errors[i].parentNode.addEventListener("click", function() {
@@ -10,6 +11,25 @@
 			});
 		}
 	}
+
+	function fadeOut() {
+		var container = document.getElementById("error-container");
+		if (!container) { 
+			return ;
+		}
+		el = container.lastChild;
+		if (!el) {
+			return ;
+		}
+		var curr = parseFloat(window.getComputedStyle(el).getPropertyValue("opacity"));
+		if (curr > 0.1) {
+			el.style.opacity = (curr - 0.1);
+		} else  {
+			el.remove();
+		}
+	}
+
+	setInterval(function() { fadeOut(); }, 500);
 
 	window.addEventListener("load", addListeners);
 })();

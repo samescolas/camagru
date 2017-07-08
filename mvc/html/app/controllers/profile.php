b6bb347c4dc05503022cb9e75969557d96c534e2
Sam Escolas <sescolas@student.42.us.org>
2017-07-07 23:55:43 -0400

    Adding profile path

--- mvc/html/app/controllers/profile.php
+++ mvc/html/app/controllers/profile.php
@@ -2,9 +2,11 @@
 
 class Profile extends Controller {
 	private $_user = null;
+	private $_db = null;
 
 	public function __construct() {
 		$this->_user = $this->model('User');
+		$this->_db = Database::getInstance();
 		$this->_user->shield();
 		$this->view('includes/header', array(
 			'stylesheets' => array('header'),
@@ -22,7 +24,7 @@ class Profile extends Controller {
 	}
 
 	public function me($data = '') {
-		echo "herE";
+		echo "<img src=\"resources/uploads/11/test.png\" >";
 	}
 }
 

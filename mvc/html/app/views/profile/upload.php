b561e0cba3469bda47528ebdbebdc3a4e6339c86
Sam Escolas <sescolas@student.42.us.org>
2017-07-10 00:43:06 -0400

    Adding change to fix uplod

--- mvc/html/app/views/profile/upload.php
+++ mvc/html/app/views/profile/upload.php
@@ -1,6 +1,6 @@
 <form action="" method="post" enctype="multipart/form-data">
 	<label for="title">Title</label><input type="text" name="title" id="title"><br />
-	<input type="file" name="fileToUpload" id="fileToUpload" />
+	<input type="file" name="data" id="data" />
 	<label for="Description">Description</label><input type="text" name="description" id="description"><br />
 	<input type="submit" value="Upload Image" name="submit" />
 </form>

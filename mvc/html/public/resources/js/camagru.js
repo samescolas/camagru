0a2449a260aabba66299f9786b6d26f99e2f2b3f
Sam Escolas <sescolas@student.42.us.org>
2017-07-10 00:39:47 -0400

    Converting to png client site

--- mvc/html/public/resources/js/camagru.js
+++ mvc/html/public/resources/js/camagru.js
@@ -90,7 +90,41 @@
     photo.setAttribute('src', data);
   }
 
+  function b64ToUint6(nChr) {
+		// convert base64 encoded character to 6-bit integer
+		// developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding
+		return (nChr > 64 && nChr < 91 ? nChr - 65
+			: nChr > 96 && nChr < 123 ? nChr - 71
+			: nChr > 47 && nChr < 58 ? nChr + 4
+			: nChr === 43 ? 62 
+			: nChr === 47 ? 63 
+			: 0);
+  }
+
+  function base64DecToArr(sBase64, nBlocksSize) {
+		// convert base64 encoded string to uIntArray
+		// developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding
+		var sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, "");
+		var nInLen = sB64Enc.length;
+		var nOutLen = nBlocksSize ? 
+				Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2;
+		var taBytes = new Uint8Array(nOutLen);
+
+		for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+			nMod4 = nInIdx & 3;
+			nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+			if (nMod4 === 3 || nInLen - nInIdx === 1) {
+				for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+					taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+				}
+				nUint24 = 0;
+			}
+		}
+		return taBytes;
+  }
+
   function sendpicture(data) {
+		var rawData = data.replace(/^data\:image\/\w+\;base64\,/, '');
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
@@ -98,8 +132,11 @@
 			}
 		};
 		xhttp.open("POST", "../capture", true);
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhttp.send(JSON.stringify(data));
+
+		var blob = new Blob( [base64DecToArr(rawData)], {type: 'image/png'} );
+		var form = new FormData();
+		form.append("data", blob, 'testimage');
+		xhttp.send(form);
   }
 
   

58da9a60b6322d8a874176de5dac11dfd873ccc7
Sam Escolas <samescolas@gmail.com>
2017-08-27 21:31:12 +0000

    Cleaning up HTML.

--- mvc/html/app/views/forms/images.php
+++ mvc/html/app/views/forms/images.php
@@ -1,12 +1,14 @@
 <div id="main-wrapper">
 	<div id="title">
 		<h1><?=$data['image']->title ?></h1>
+		<div id="settings-img-container">
 		<?php if ($data['mine']) {
-			echo "<img id=\"settings-img\" src=\"resources/imgs/settings.png\"/>";} 
+			echo "<img id=\"settings-img\" src=\"resources/imgs/settings.png\"/ align=\"right\">";} 
 		?>
+		</div>
 	</div>
-	<div id="image">
-		<img id="main-img" src="<?=$data['image']->filepath ?>" width="25%">
+	<div id="image-container">
+		<img id="main-img" src="<?=$data['image']->filepath ?>" >
 	</div>
 	<p id="likes"><?=$data['image']->likes ?>&hearts;</p>
 	<div id="comments">

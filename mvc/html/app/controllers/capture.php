b5d81afbe4ad2516a3ff8f3abc6e56a75b25af6b
Sam Escolas <samescolas@gmail.com>
2017-08-29 01:00:33 +0000

    Adding CAMAGRU as default title for all images.

--- mvc/html/app/controllers/capture.php
+++ mvc/html/app/controllers/capture.php
@@ -14,7 +14,7 @@ class Capture extends Controller {
 	public function index($data = '') {
 		if (Input::exists('file')) {
 			$image = $this->model('Image', array(
-				'title' => 'Screenshot 1',
+				'title' => 'CAMAGRU',
 				'description' => 'Captured ' . date('Y-m-d H:m:s', time()),
 				'user_id' => $this->_user->data()->id
 			));
